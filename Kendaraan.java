4ef53d3c63fa0cca341970ec3b1a94f6fa70b5cc
iqbaltif14 <iqbal.tif14@gmail.com>
2017-03-26 15:25:35 +0700

    Add files via upload

---
+++ Kendaraan.java
@@ -0,0 +1,24 @@
+public class Kendaraan {
+    public static void main(String[] args) {
+        Sepeda sepeda1 = new Sepeda();
+        
+        System.out.println("Gigi Sepeda 1 = "+sepeda1.getGigi());
+        System.out.println("Merk Sepeda 1 = "+sepeda1.getMerk());
+        
+        System.out.println("Rubah Sepeda 1");
+        sepeda1.setGigi(4);
+        sepeda1.setMerk("Polygon");        
+        System.out.println("Gigi Sepeda 1 = "+sepeda1.getGigi());
+        System.out.println("Merk Sepeda 1 = "+sepeda1.getMerk());
+        
+        Sepeda sepeda2 = new Sepeda(6,"Monster");
+        System.out.println("Gigi Sepeda 2 = "+sepeda2.getGigi());
+        System.out.println("Merk Sepeda 2 = "+sepeda2.getMerk());
+        
+        System.out.println("Rubah Sepeda 2");
+        sepeda2.setGigi(12);
+        sepeda2.setMerk("Balap");        
+        System.out.println("Gigi Sepeda 2 = "+sepeda2.getGigi());
+        System.out.println("Merk Sepeda 2 = "+sepeda2.getMerk());
+    }
+}
